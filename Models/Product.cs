955b535f20658835642497363aa0d1c787551a82
Lukas Terwingen <lterwingen@selligent.com>
2020-04-18 01:12:05 +0200

    HomePage first go

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Blazor.Models
+{
+    public class Product
+    {
+        public string id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Average_rating { get; set;}
+        public ReviewModel[] reviews { get; set; }
+        public string Desc { get; set; }
+        public decimal Price { get; set; }
+        public string image_src { get; set; }
+    }
+}
---
+++ Models/ReviewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Models
+{
+    public class ReviewModel
+    {
+        public string id { get; set; }
+        public string author_name { get; set; }
+        public string title { get; set; }
+        public DateTime timestamp { get; set; }
+        public int rating { get; set; }
+        public string comment { get; set; }
+    }
+}
